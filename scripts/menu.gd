a0e7435d03f9c777c20b7aa662787825fed92e76
phamquyetthang <17021033@vnu.edu.vn>
2020-05-17 17:00:48 +0700

    them menu, backgroud

---
+++ scripts/menu.gd
@@ -0,0 +1,18 @@
+extends Node
+
+func _ready():
+	$MarginContainer/VBoxContainer/VBoxContainer/StartButton.grab_focus()
+
+func _physics_process(delta):
+	if $MarginContainer/VBoxContainer/VBoxContainer/StartButton.is_hovered() == true:
+		$MarginContainer/VBoxContainer/VBoxContainer/StartButton.grab_focus()
+	if $MarginContainer/VBoxContainer/VBoxContainer/ExitButton.is_hovered() == true:
+		$MarginContainer/VBoxContainer/VBoxContainer/ExitButton.grab_focus()
+
+
+func _on_StartButton_pressed():
+	get_tree().change_scene("res://scenes/world.tscn")
+
+
+func _on_ExitButton_pressed():
+	get_tree().quit()
